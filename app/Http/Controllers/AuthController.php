6f46d9dc1a9c8eebecc31d9a4ed694a73623f68d
Victor Edet <edetvictor849@gmail.com>
2022-01-18 09:00:03 +0100

    (GZ)Chore: genzee backend init

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Auth\Events\Registered;
+// use App\Mail\WelcomeMail;
+use Illuminate\Support\Facades\Mail;
+
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'account' => 0,
+            'password' => bcrypt($fields['password']),
+
+        ]);
+        
+        $token = $user-> createToken('myapptoken')->plainTextToken;
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+        
+        
+        // Mail::to($fields['email'])->send(new WelcomeMail($user));
+        event(new Registered($user));
+        return response($response, 201);
+    }
+
+    public function verify(Request $request, $id)
+    {
+        $user = User::find($id);
+        User::where('id',$id)->update([
+            'email_verified'=>'yes'
+        ]);
+       
+        return 'your account has been verified'      ;  
+    }
+
+    public function login(Request $request) {
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        // Check email
+        $user = User::where('email', $fields['email'])->first();
+
+        // Check password
+        if(!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => 'Bad credentails'
+            ], 401);
+        }
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function logout(Request $request) {
+        auth()->user()->tokens()->delete();
+
+        return [
+            'message' => 'Logged out'
+        ];
+    }
+}

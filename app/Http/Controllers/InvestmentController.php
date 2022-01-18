ce211419c9babbf0afaff77bb4bf551d69772b04
Victor Edet <edetvictor849@gmail.com>
2022-01-18 17:40:27 +0100

    (GZ)Chore: transfer profit to account

--- app/Http/Controllers/InvestmentController.php
+++ app/Http/Controllers/InvestmentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Withdraw;
 use App\Models\Investment;
 
 
@@ -45,13 +46,32 @@ class InvestmentController extends Controller
 
 
         // return user's investments
+        return Investment::where('user_id',$id)->get();
     }
 
     
-    public function store(Request $request)
+    public function transferProfit(Request $request, $id)
     {
-        //
-    }
+        //get investment
+        $investment = Investment::where('id', $id)->get();
+        //get user id
+        $user_id = $investment['user_id'];
+        
+         //get user account
+         $user = User::where('id',$user_id)->get();
+         $new_profit = $user['profit']+$investment['profit'];
+         
+         //update user account
+        User::where('id', $user_id)->update([
+            'profit'=>$new_profit
+        ]);
+        //update investment
+        Investment::where('id', $id)->update([
+            'profit'=>0
+        ]);
+        //return user's investment
+        return Investment::where('user_id', $user_id)->get();
+    }   
 
     public function show($id)
     {
